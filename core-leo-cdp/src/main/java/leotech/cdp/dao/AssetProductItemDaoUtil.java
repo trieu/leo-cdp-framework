bd2dcf6bc2dcca007b29ee431e645e27154d3011
trieu <tantrieuf31.database@gmail.com>
2026-02-05 18:08:43 +0700

    fix bugs upsert product items

--- core-leo-cdp/src/main/java/leotech/cdp/dao/AssetProductItemDaoUtil.java
+++ core-leo-cdp/src/main/java/leotech/cdp/dao/AssetProductItemDaoUtil.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import com.arangodb.ArangoCollection;
 import com.arangodb.ArangoDatabase;
+import com.arangodb.model.AqlQueryOptions;
 
 import leotech.cdp.model.asset.ProductItem;
 import leotech.cdp.model.marketing.TargetMediaUnit;
@@ -16,7 +17,6 @@ import leotech.system.util.database.ArangoDbCommand;
 import leotech.system.util.database.ArangoDbCommand.CallbackQuery;
 import leotech.system.util.database.ArangoDbUtil;
 import rfx.core.util.StringUtil;
-
 
 /**
  * Product Item Database
@@ -30,72 +30,86 @@ public class AssetProductItemDaoUtil extends AssetItemDao {
 	static final String PRODUCT_ITEM = "product-item";
 
 	static final String AQL_FIND_KEY_AQL = ArangoDbUtil.contentFindKeyAql(ProductItem.COLLECTION_NAME);
-	
+
 	static final String AQL_GET_PRODUCT_ITEMS_IN_GROUP = AqlTemplate.get("AQL_GET_PRODUCT_ITEMS_IN_GROUP");
 	static final String AQL_COUNT_PRODUCT_ITEMS_IN_GROUP = AqlTemplate.get("AQL_COUNT_PRODUCT_ITEMS_IN_GROUP");
-	
+
 	static final String AQL_GET_PRODUCT_ITEM_BY_ID = AqlTemplate.get("AQL_GET_PRODUCT_ITEM_BY_ID");
 	static final String AQL_GET_PRODUCT_ITEM_BY_FULL_URL = AqlTemplate.get("AQL_GET_PRODUCT_ITEM_BY_FULL_URL");
-	static final String AQL_GET_PRODUCT_ITEM_BY_PRODUCT_ID_AND_ID_TYPE = AqlTemplate.get("AQL_GET_PRODUCT_ITEM_BY_PRODUCT_ID_AND_ID_TYPE");
-	
+	static final String AQL_GET_PRODUCT_ITEM_BY_PRODUCT_ID_AND_ID_TYPE = AqlTemplate
+			.get("AQL_GET_PRODUCT_ITEM_BY_PRODUCT_ID_AND_ID_TYPE");
+
 	static final String AQL_GET_PRODUCT_ITEMS = AqlTemplate.get("AQL_GET_PRODUCT_ITEMS");
 	static final String AQL_SEARCH_PRODUCT_ITEMS_BY_KEYWORDS = AqlTemplate.get("AQL_SEARCH_PRODUCT_ITEMS_BY_KEYWORDS");
 	static final String AQL_GET_PRODUCT_ITEMS_BY_CAMPAIGN = AqlTemplate.get("AQL_GET_PRODUCT_ITEMS_BY_CAMPAIGN");
 
-	/**
-	 * @param ProductItem
-	 * @return
-	 */
 	public static String save(ProductItem item) {
-		if (item.dataValidation()) {
-			ArangoDatabase db = getCdpDatabase();
-			ArangoCollection col = item.getDbCollection();
-			if (col != null) {
-				String id = item.getId();
-				
-				TargetMediaUnit targetMediaUnit = item.createTargetMediaUnit();
-				if(targetMediaUnit != null) {
-					String mid = TargetMediaUnitDaoUtil.save(targetMediaUnit);
-					if(StringUtil.isNotEmpty(mid)) {
-						item.createShortLinkUrlFromTargetMediaUnit(mid);
-						item.createQrCodeUrlFromShortLinkUrl(PRODUCT_ITEM);
-					}
-				}
-				item.setUpdatedAt(new Date());
-				col.insertDocument(item, optionToUpsertInSilent());
-				
-				return id;
-			} else {
-				System.err.println("AssetProductItemDaoUtil save ArangoCollection is NULL");
+
+		if (!item.dataValidation()) {
+			return null;
+		}
+
+		ArangoDatabase db = getCdpDatabase();
+		ArangoCollection col = item.getDbCollection();
+
+		if (col == null) {
+			System.err.println("AssetProductItemDaoUtil save ArangoCollection is NULL");
+			return null;
+		}
+
+		String id = item.getId();
+
+		// ---- side effects ----
+		TargetMediaUnit targetMediaUnit = item.createTargetMediaUnit();
+		if (targetMediaUnit != null) {
+			String mid = TargetMediaUnitDaoUtil.save(targetMediaUnit);
+			if (StringUtil.isNotEmpty(mid)) {
+				item.createShortLinkUrlFromTargetMediaUnit(mid);
+				item.createQrCodeUrlFromShortLinkUrl(PRODUCT_ITEM);
 			}
 		}
-		return null;
+
+		item.setUpdatedAt(new Date());
+
+		// ---- REAL UPSERT (silent, atomic) ----
+		String aql = "UPSERT { _key: @key } " + "INSERT @doc " + "UPDATE @doc " + "IN @@col "
+				+ "OPTIONS { silent: true }";
+
+		Map<String, Object> bindVars = new HashMap<>();
+		bindVars.put("@col", col.name());
+		bindVars.put("key", id);
+		bindVars.put("doc", item);
+
+		db.query(aql, bindVars, new AqlQueryOptions(), Void.class);
+
+		return id;
 	}
 
 	public static ProductItem getById(String id) {
 		return getById(id, true);
 	}
-	
+
 	public static ProductItem getById(String id, boolean loadCategoryData) {
-		if(StringUtil.isNotEmpty(id)) {
+		if (StringUtil.isNotEmpty(id)) {
 			ArangoDatabase db = getCdpDatabase();
 			Map<String, Object> bindVars = new HashMap<>(1);
 			bindVars.put("id", id);
-			ProductItem p = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEM_BY_ID, bindVars, ProductItem.class, new CallbackQuery<ProductItem>() {
-				@Override
-				public ProductItem apply(ProductItem obj) {
-					obj.buildDefaultHeadlineImage();
-					if(loadCategoryData) {
-						loadGroupsAndCategory(obj);
-					}
-					return obj;
-				}
-			}).getSingleResult();
+			ProductItem p = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEM_BY_ID, bindVars,
+					ProductItem.class, new CallbackQuery<ProductItem>() {
+						@Override
+						public ProductItem apply(ProductItem obj) {
+							obj.buildDefaultHeadlineImage();
+							if (loadCategoryData) {
+								loadGroupsAndCategory(obj);
+							}
+							return obj;
+						}
+					}).getSingleResult();
 			return p;
 		}
 		return null;
 	}
-	
+
 	public static boolean delete(String id) {
 		ArangoCollection col = ProductItem.getCollection();
 		if (col != null) {
@@ -109,16 +123,18 @@ public class AssetProductItemDaoUtil extends AssetItemDao {
 		ArangoDatabase db = getCdpDatabase();
 		Map<String, Object> bindVars = new HashMap<>(1);
 		bindVars.put("url", url);
-		ProductItem p = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEM_BY_FULL_URL, bindVars, ProductItem.class).getSingleResult();
+		ProductItem p = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEM_BY_FULL_URL, bindVars,
+				ProductItem.class).getSingleResult();
 		return p;
 	}
-	
+
 	public static ProductItem getByProductId(String productId, String productIdType) {
 		ArangoDatabase db = getCdpDatabase();
 		Map<String, Object> bindVars = new HashMap<>(2);
 		bindVars.put("productId", productId);
 		bindVars.put("productIdType", productIdType);
-		ProductItem p = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEM_BY_PRODUCT_ID_AND_ID_TYPE, bindVars, ProductItem.class).getSingleResult();
+		ProductItem p = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEM_BY_PRODUCT_ID_AND_ID_TYPE, bindVars,
+				ProductItem.class).getSingleResult();
 		return p;
 	}
 
@@ -127,10 +143,11 @@ public class AssetProductItemDaoUtil extends AssetItemDao {
 		Map<String, Object> bindVars = new HashMap<>(2);
 		bindVars.put("startIndex", startIndex);
 		bindVars.put("numberResult", numberResult);
-		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEMS, bindVars,ProductItem.class).getResultsAsList();
+		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEMS, bindVars,
+				ProductItem.class).getResultsAsList();
 		return list;
 	}
-	
+
 	/**
 	 * @param keywords
 	 * @param startIndex
@@ -143,33 +160,34 @@ public class AssetProductItemDaoUtil extends AssetItemDao {
 		bindVars.put("keywords", "%" + keywords + "%");
 		bindVars.put("startIndex", startIndex);
 		bindVars.put("numberResult", numberResult);
-		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_SEARCH_PRODUCT_ITEMS_BY_KEYWORDS, bindVars,ProductItem.class).getResultsAsList();
+		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_SEARCH_PRODUCT_ITEMS_BY_KEYWORDS, bindVars,
+				ProductItem.class).getResultsAsList();
 		Map<String, String> map = new HashMap<>(list.size());
 		for (ProductItem productItem : list) {
 			String title = productItem.getTitle();
-			if(title.length()>100) {
+			if (title.length() > 100) {
 				title = title.substring(0, 100) + "...";
 			}
 			map.put(productItem.getProductId(), title);
 		}
 		return map;
 	}
-	
+
 	public static List<ProductItem> getByCampaign(String campaignId) {
 		ArangoDatabase db = getCdpDatabase();
 		Map<String, Object> bindVars = new HashMap<>(1);
 		bindVars.put("campaignId", campaignId);
-		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEMS_BY_CAMPAIGN, bindVars, ProductItem.class).getResultsAsList();
+		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEMS_BY_CAMPAIGN, bindVars,
+				ProductItem.class).getResultsAsList();
 		return list;
 	}
 
 	public static List<ProductItem> list(String searchValue, String groupId, int startIndex, int numberResult) {
 		ArangoDatabase db = getCdpDatabase();
 		Map<String, Object> bindVars = new HashMap<>(4);
-		if(searchValue.length()>1) {
+		if (searchValue.length() > 1) {
 			bindVars.put("keywords", searchValue);
-		}
-		else {
+		} else {
 			bindVars.put("keywords", "");
 		}
 		bindVars.put("groupId", groupId);
@@ -178,21 +196,22 @@ public class AssetProductItemDaoUtil extends AssetItemDao {
 
 		LogUtil.logInfo(AssetProductItemDaoUtil.class, bindVars);
 		LogUtil.logInfo(AssetProductItemDaoUtil.class, AQL_GET_PRODUCT_ITEMS_IN_GROUP);
-		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEMS_IN_GROUP, bindVars, ProductItem.class).getResultsAsList();
+		List<ProductItem> list = new ArangoDbCommand<ProductItem>(db, AQL_GET_PRODUCT_ITEMS_IN_GROUP, bindVars,
+				ProductItem.class).getResultsAsList();
 		return list;
 	}
-	
+
 	public static int countTotalSizeOfGroup(String searchValue, String groupId) {
-		ArangoDatabase db = getCdpDatabase();		
+		ArangoDatabase db = getCdpDatabase();
 		Map<String, Object> bindVars = new HashMap<>(2);
 		bindVars.put("groupId", groupId);
-		if(searchValue.length()>1) {
+		if (searchValue.length() > 1) {
 			bindVars.put("keywords", "%" + searchValue + "%");
-		}
-		else {
+		} else {
 			bindVars.put("keywords", "");
 		}
-		Integer count = new ArangoDbCommand<Integer>(db, AQL_COUNT_PRODUCT_ITEMS_IN_GROUP, bindVars, Integer.class).getSingleResult();
+		Integer count = new ArangoDbCommand<Integer>(db, AQL_COUNT_PRODUCT_ITEMS_IN_GROUP, bindVars, Integer.class)
+				.getSingleResult();
 		return count;
 	}
 
