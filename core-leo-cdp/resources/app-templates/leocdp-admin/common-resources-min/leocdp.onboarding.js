b7337453bf44481054e375657e6167f677b56497
trieu <trieu@leocdp.com>
2026-08-22 14:00:00 +0700

    update build

--- core-leo-cdp/resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ core-leo-cdp/resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2026-06-27-10-24-21
+// Version: v_0.9.0_2026-08-22-13-59-19
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};
