b55aaddd03efe86d2c6eb20f93cc6d9fac888431
trieu <tantrieuf31.database@gmail.com>
2026-09-17 16:13:54 +0700

    Update version to v_0.9.0_2026-09-17-16-07-01 and refactor message handling in leo-event-proxy.html; enhance segment details and system scripts for improved functionality and performance.

--- core-leo-cdp/resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ core-leo-cdp/resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2026-08-22-13-59-19
+// Version: v_0.9.0_2026-09-17-16-07-01
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};
